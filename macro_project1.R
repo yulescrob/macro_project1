531d383bf61f8c222490e01107fc748ccd913ff1
yulescrob <yulescrob@gmail.com>
2025-01-26 11:03:17 -0500

    created plots for GDP growth rate and average growth rate for Bangladesh

--- macro_project1.R
+++ macro_project1.R
@@ -1,8 +1,34 @@
+#Macroeconomics Theory Project 1
+
+#set your working directory
+
 #loading the data
 library(readr)
-project1_data <- read_csv("~/Library/CloudStorage/OneDrive-UniversityofCincinnati/Macro/project1_data.csv")
+project1_data <- read_csv("project1_data.csv")
 View(project1_data)
 
 #changing missing values to NA
-NA->data_project1$hci[data_project1$hci == ".."] 
-NA-> data_project1$laborforcert[data_project1$laborforcert == ".."]
+NA->project1_data$hci[project1_data$hci == ".."] 
+NA->project1_data$laborforcert[project1_data$laborforcert == ".."]
+
+
+#sorting the by country and year
+library(dplyr)
+data <- project1_data %>%
+  arrange(country, time) %>%
+  group_by(country) %>%
+  mutate(growth_rate = (GDP_pp/ lag(GDP_pp)-1)*100) #calculates growth rate
+
+#creating growth rate graphs
+library(ggplot2)
+
+growth_plot <- ggplot(data, aes(x = time, y = growth_rate, color = country)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 3) +
+  labs(title = "GDP Per Capita Growth Rates",
+    x = "Year",
+    y = "Growth Rate (%)",
+    color = "Country"
+  ) +
+  theme_classic()
+
